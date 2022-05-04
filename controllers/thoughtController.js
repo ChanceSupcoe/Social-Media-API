fbed7004219d9edc32cfcb7187b98ce9596b6daf
Chance Supcoe <chancesupcoe@gmail.com>
2022-05-04 14:03:50 -0400

    thought controller/route 75%

--- controllers/thoughtController.js
+++ controllers/thoughtController.js
@@ -2,17 +2,90 @@ const {User, Thought} = require('../models');
 
 const ThoughtController = {
 
-    getAllThoughts
+    getAllThoughts(req, res) {
+        Thought.find({})
+        .populate({path: 'reactions', select: '-__v'})
+        .select('-__v')
+        .then(dbThoughtData => res.json(dbThoughtData))
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        })
+    },
 
-    getThoughtById
+    getThoughtById({params}, res) {
+        Thought.findOne({_id:params.id})
+        .populate({path: 'reactions', select: '-__v'})
+        .select('-__v')
+        .then(dbThoughtData => {
+            if(!dbThoughtData) {
+                res.status(404).json({message: 'Thought not found'});
+                return;
+            }
+            res.json(dbThoughtData);
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(400).json(err);
+        });
+    },
 
-    postNewThought
+    postNewThought({body}, res) {
+        Thought.create(body)
+        .then(dbThoughtData => {
+            User.findOneAndUpdate(
+                {_id: body.userId},
+                {$push: {thoughts: dbThoughtData._id}},
+                {new: true}
+            )
+            .then (dbUserData => {
+                if (!dbUserData) {
+                    res.status(404).json({message: 'User not found'});
+                    return;
+                }
+                res.json(dbUserData);
+            })
+            .catch(err => res.status(400).json(err));
+        })
+        .catch(err => res.status(400).json(err));
+    },
 
-    putUpdateThought
+    putUpdateThought({params, body}, res) {
+        Thought.findOneAndUpdate(
+            {_id:params.id},
+            body,
+            {new: true}
+        )
+        .then(dbThoughtData => {
+            if (!dbThoughtData) {
+                res.status(404).json({message: 'Thought not found'});
+                return;
+            }
+            res.json(dbThoughtData);
+        })
+        .catch(err => res.status(400).json(err));
+    },
 
-    deleteThought
+    deleteThought({params}, res) {
+        Thought.findOneAndDelete({_id:params.id})
+        .then(dbThoughtData => {
+            if (!dbThoughtData) {
+                res.status(404).json({message: 'Thought not found'});
+                return;
+            }
+            User.findOneAndUpdate(
+                {username: dbThoughtData.username},
+                {$pull: {thoughts: params.id}}
+            )
+            .then(() => {
+                res.json({message: 'Thought Deleted'});
+            })
+            .catch(err => res.status(500).json(err));
+        })
+        .catch(err => res.status(500).json(err));
+    },
 
-    
+
 
 
 };
--- routes/api/thoughtRoute.js
+++ routes/api/thoughtRoute.js
@@ -1,2 +1,19 @@
 const router = require('express').Router();
 
+const {
+    getAllThoughts,
+    getThoughtById,
+    postNewThought,
+    putUpdateThought,
+    deleteThought
+    } =require('../../controllers/thoughtController');
+
+router.route('/')
+    .get(getAllThoughts)
+    .post(postNewThought)
+
+router.route('/:id')
+    .get(getThoughtById)
+    .put(putUpdateThought)
+    .delete(deleteThought)
+
